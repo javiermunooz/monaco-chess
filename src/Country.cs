b9d3a7d04aca35cdbcb35395f477f94d9d3bef9c
fjavier97vall <fjavier97vall@gmail.com>
2020-09-17 17:55:37 +0200

    minor additions to Player Class

---
+++ src/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace monaco_chess.src
+{
+    enum Country
+    {
+        Spain,
+        Ireland,
+        United Kingdom
+    }
+}
--- src/Player.cs
+++ src/Player.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace monaco_chess.src
 {
     class Player
     {
+        private string lastName { get; set; }
+        private string firstName { get; set; }
+        private Club club { get; set; }
+        private int fideID { get; set; }
+        private Country country { get; set; }
+        private DateTime yearBirth { get; set; }
+        private int rating { get; set; }
+        private float points { get; set; }
+        private float des1 { get; set; }
+        private float des2 { get; set; }
+        private float des3 { get; set; }
+        private List<Player> opponentList { get; }
     }
 }
